32f8ea3b8bb0ac4c4046b9ed4bad297f7aa337aa
LegrandMichael <michael.c.legrand@gmail.com>
2018-01-11 14:59:29 +0100

    Create Category class and repository

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cat_name", type="string", length=255)
+     */
+    private $catName;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set catName.
+     *
+     * @param string $catName
+     *
+     * @return Category
+     */
+    public function setCatName($catName)
+    {
+        $this->catName = $catName;
+
+        return $this;
+    }
+
+    /**
+     * Get catName.
+     *
+     * @return string
+     */
+    public function getCatName()
+    {
+        return $this->catName;
+    }
+}
