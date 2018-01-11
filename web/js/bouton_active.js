b5c0a2b125c77429ee4ca998871f5c165ce59b19
Fendrir <papao1@hotmail.fr>
2018-01-11 15:15:41 +0100

    première version non fonctionnelle du modul des active des boutons de la nav bar

--- web/js/bouton_active.js
+++ web/js/bouton_active.js
@@ -1,22 +1,48 @@
-//fonction de récupération de donnée dans l'URL
-function GetURLParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
+$(document).ready(function(){
+    activerOnglet();
+});
+     
+function activerOnglet()
+{
+        // Il faut tout d'abord récupérer le nom de l'onglet où on se trouve.
+        // Pour moi, je peux savoir l'onglet grâce à l'URL
+        // Mes URLs sont de la forme localhost/monprojet/page1/truc
+        // Je dois donc récupérer l'élément après "monprojet".
+ 
+    var url=document.location.href.split("/"); // Je sépare selon les '/' de l'URL
+    var indiceMonProjet = array_search("monprojet",url); //Je récupère l'endroit où se situe 'monprojet'
+    var indiceOngletAppele = indiceMonProjet +1; // L'onglet que je cherche est donc à indiceMonProjet+1
+ 
+    var ongletAppele = url[indiceOngletAppele]; //Je récupère le nom de l'onglet actif (page1)
+    var ongletActif;
+     
+        // Traitement selon l'onglet récupéré
+        if( ongletAppele == 'page1')
     {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] === sParam)
+        ongletActif = document.getElementById('id_li_page1');
+    }
+    else if (ongletAppele == 'page2')
+    {
+        ongletActif = document.getElementById('id_li_page2');
+    }
+    else // Sinon, je rends actif mon onglet "Home"
+    {
+        ongletActif = document.getElementByID('id_li_home');
+    }
+     
+    ongletActif.className += "active";
+}
+ 
+function array_search(what, where){
+    var index_du_tableau=-1;
+    for(elt in where)
+    {
+        index_du_tableau++;
+        if (where[elt]==what)
         {
-            return sParameterName[1];
+            return index_du_tableau;
         }
     }
+    index_du_tableau=-1;
+    return index_du_tableau;
 }
-
-
-//On se sert de ce même système pour ajouter la classe .active aux button du menu en se servant de leurs ID
-var menuButtonId = GetURLParameter('p');
-//Si le visiteur vient d'arriver sur le site, 'p' ne sera pas égal à home, donc on set active manuellement
-if (!menuButtonId){
-    $('#home').addClass('active');
-}
-$('#' + menuButtonId).addClass('active');
